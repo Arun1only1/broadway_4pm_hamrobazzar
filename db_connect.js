6cd77b504c3a8a3d58d7c43ff3175e7084f844b4
Arun1only1 <arun1nly1@gmail.com>
2023-08-04 17:40:04 +0545

    feat: env setup and encode uri implementation

--- db_connect.js
+++ db_connect.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
-// create db_url
-const db_url = `mongodb+srv://${DB_USERNAME}:${encodeURIComponent(
-  DB_PASSWORD
-)}@school.b6qkdnb.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
 export const db_connect = async () => {
   try {
-    await mongoose.connect(db_url);
+    await mongoose.connect(
+      `mongodb+srv://${DB_USERNAME}:${encodeURIComponent(
+        DB_PASSWORD
+      )}@school.b6qkdnb.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    );
+
     console.log("DB connection OK");
   } catch (error) {
     console.log("DB connection failed");
